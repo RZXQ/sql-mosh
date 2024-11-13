446942962b0eb5ed9dc8168280b515be0436c7be
Reacher <macryze@icloud.com>
2024-11-13 14:40:20 +0800

    Simplify SQL comments for compound join conditions
    Removed verbose step-by-step instructions and clarified SQL comments. This enhances readability by providing succinct explanations directly above the relevant SQL commands.

--- 3- Retrieving Data From Multiple Tables/5- Compound Join Conditions/5- Compound Join Conditions.sql
+++ 3- Retrieving Data From Multiple Tables/5- Compound Join Conditions/5- Compound Join Conditions.sql
@@ -1,13 +1,9 @@
--- 1. Switch to the specific database 'sql_store'
+-- Selects all columns from 'order_items' table
+-- Joins 'order_items' with 'order_item_notes' table from 'sql_store' database
+-- On condition: matching both 'order_id' and 'product_id' in both tables as a composite key
 USE sql_store;
-
--- 2. Retrieve all columns from the 'order_items' table.
--- 3. Perform an INNER JOIN with the 'order_item_notes' table
---    - The JOIN is based on two conditions:
---        a. 'order_id' columns from both tables must match
---        b. 'product_id' columns from both tables must match
 SELECT *
-FROM order_items oi -- Alias for order_items table (oi)
-         JOIN order_item_notes oin -- Alias for order_item_notes table (oin)
-              ON oi.order_id = oin.order_Id -- Condition: Matching order_id
-                  AND oi.product_id = oin.product_id; -- Condition: Matching product_id (compound join condition)
+FROM order_items oi
+         JOIN order_item_notes oin
+              ON oi.order_id = oin.order_id
+                  AND oi.product_id = oin.product_id;
