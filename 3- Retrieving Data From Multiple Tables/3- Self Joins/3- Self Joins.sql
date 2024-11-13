04e1ed3c3d611b0fa16a478b7b1070cb96a8a725
Reacher <macryze@icloud.com>
2024-11-13 14:11:00 +0800

    Refactor self-join query in SQL script
    Updated the SQL script to include comments for clarity and refined the query structure for readability. Changed 'JOIN' to 'WHERE' to match the join condition with the conventional format.

--- 3- Retrieving Data From Multiple Tables/3- Self Joins/3- Self Joins.sql
+++ 3- Retrieving Data From Multiple Tables/3- Self Joins/3- Self Joins.sql
@@ -1,11 +1,9 @@
--- 1. Select the hr database
 USE sql_hr;
 
--- Select the employee's ID and first name
-SELECT e.employee_id,
-       e.first_name,
-       m.first_name AS manager
--- The main employees table is alias as 'e'
+-- Selects 'employee_id' and 'first_name' from 'employees' table as employee details
+-- Joins 'employees' table with itself to get manager details
+-- On condition: 'reports_to' in employee matches 'employee_id' in manager
+SELECT e.employee_id, e.first_name, m.first_name AS manager
 FROM employees e
          JOIN employees m
-              ON e.reports_to = m.employee_id;
+WHERE e.reports_to = m.employee_id;
