dccc739d1a72891dc9e84f224d816aeab04552c1
Reacher <macryze@icloud.com>
2024-11-16 14:56:24 +0800

    Simplify comments and update SQL joins
    Revised the explanation for outer joins for clarity and simplified the SQL join queries. Removed redundant SELECT-ORDER commands and standardized JOIN conditions for consistency.

--- 3- Retrieving Data From Multiple Tables/7- Outer Joins/7- Outer Joins.sql
+++ 3- Retrieving Data From Multiple Tables/7- Outer Joins/7- Outer Joins.sql
@@ -1,11 +1,12 @@
--- How Outer Joins work:
--- 1. Cross Join: First, it creates what is known as a Cartesian product (cross join) of the two participating tables.
---    This means that each row from the left table is combined with each row from the right table.
--- 2. Apply the 'ON' Clause: The next step is to apply the join condition specified in the 'ON' clause.
---    It filters the Cartesian product to only include rows where the condition evaluates to 'TRUE'.
--- 3. Include All Left Rows: For rows in the left table that do not find any matching rows in the right table
---    based on the 'ON' condition, the result will include these rows with 'NULL' values for columns from the right table.
---    Essentially, the 'LEFT JOIN' ensures that all rows from the left table appear in the result set.
+-- How Outer Joins Work:
+-- 1. Start with all rows from the left table.
+-- 2. For each row in the left table, attempt to find matching rows in the right table based on the 'ON' condition.
+-- 3. If a match is found:
+--    - Combine the left table row with the matching right table row(s).
+-- 4. If no match is found:
+--    - Combine the left table row with NULLs for all columns from the right table.
+-- This process ensures that all rows from the left table are included in the result set,
+-- with corresponding data from the right table when available.
 
 USE sql_store;
 
@@ -13,19 +14,13 @@ USE sql_store;
 -- Joins 'customers' with 'orders' using INNER JOIN
 -- On condition: matching 'customer_id' in both tables
 -- Note: INNER JOIN excludes customers without orders
-SELECT c.customer_id, c.first_name, o.order_id
+SELECT c.first_name, o.order_id
 FROM customers c
-         INNER JOIN orders o ON c.customer_id = o.customer_id
-ORDER BY c.customer_id;
-
+         LEFT JOIN orders o ON c.customer_id = o.customer_id;
 -- Selects 'customer_id' and 'first_name' from 'customers' table, 'order_id' from 'orders' table
 -- Joins 'customers' with 'orders' using LEFT JOIN
 -- On condition: matching 'customer_id' in both tables
 -- Note: LEFT JOIN includes all customers, even those without orders
-SELECT c.customer_id, c.first_name, o.order_id
-FROM customers c
-         LEFT JOIN orders o ON c.customer_id = o.customer_id
-ORDER BY c.customer_id;
 
 -- Selects 'customer_id' and 'first_name' from 'customers' table, 'order_id' from 'orders' table
 -- Joins 'customers' with 'orders' using RIGHT JOIN
@@ -33,9 +28,7 @@ ORDER BY c.customer_id;
 -- Note: RIGHT JOIN includes all orders, even if there are no matching customers
 SELECT c.customer_id, c.first_name, o.order_id
 FROM customers c
-         RIGHT JOIN orders o ON c.customer_id = o.customer_id
-ORDER BY c.customer_id;
-
+         RIGHT JOIN orders o ON c.customer_id = o.customer_id;
 -- Selects 'product_id' and 'name' from 'products' table, 'quantity' from 'order_items' table
 -- Joins 'products' with 'order_items' using LEFT JOIN
 -- On condition: matching 'product_id' in both tables
