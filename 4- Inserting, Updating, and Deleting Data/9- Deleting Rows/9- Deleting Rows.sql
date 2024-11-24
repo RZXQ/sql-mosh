1123d9d1a50d1e5131d3463d8e9e9c41d6ea429f
Reacher <macryze@icloud.com>
2024-11-25 01:44:40 +0800

    Add specific and conditional DELETE statements to SQL script
    Included commands to delete a particular invoice by its ID and all invoices for a client named 'MyWorks' using a subquery. Added a command to switch to the "sql_invoicing" database at the beginning.

--- 4- Inserting, Updating, and Deleting Data/9- Deleting Rows/9- Deleting Rows.sql
+++ 4- Inserting, Updating, and Deleting Data/9- Deleting Rows/9- Deleting Rows.sql
@@ -1,9 +1,13 @@
+-- Switch to the "sql_invoicing" database
 USE sql_invoicing;
 
+-- Delete a specific invoice with invoice_id = 1 from the "invoices" table
 DELETE
 FROM invoices
 WHERE invoice_id = 1;
 
+-- Delete all invoices associated with the client whose name is 'MyWorks'
+-- The inner query selects the client_id of the client named 'MyWorks'
 DELETE
 FROM invoices
 WHERE client_id = (SELECT client_id FROM clients WHERE name = 'MyWorks');
