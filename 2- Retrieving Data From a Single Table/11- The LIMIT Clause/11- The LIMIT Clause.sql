8c028884e741d9b2baa028e9634219479d40b0b7
Reacher <macryze@icloud.com>
2024-12-07 11:51:40 +0800

    Add database context to SQL LIMIT examples
    Include the 'use sql_store' statement in SQL scripts to specify the database context, ensuring accurate execution of SQL queries. This update enhances the clarity and functionality of the examples by explicitly defining the target database.

--- 2- Retrieving Data From a Single Table/11- The LIMIT Clause/11- The LIMIT Clause.sql
+++ 2- Retrieving Data From a Single Table/11- The LIMIT Clause/11- The LIMIT Clause.sql
@@ -6,16 +6,19 @@ Summary:
 */
 
 -- Select the first 3 customers.
+use sql_store;
 SELECT *
 FROM customers
 LIMIT 3;
 
 -- Select 3 customers starting from the 7th record (implementing pagination).
+use sql_store;
 SELECT *
 FROM customers
 LIMIT 6, 3;
 
 -- Select the top 3 customers sorted by points in descending order.
+use sql_store;
 SELECT *
 FROM customers
 ORDER BY points DESC
