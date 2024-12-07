4e7558464720f1df8823038db844c254b855f075
Reacher <macryze@icloud.com>
2024-12-07 09:32:10 +0800

    Refactor SQL script by removing redundant comment.
    The comment duplicating the 'USE sql_store;' statement was removed, while the script maintains the main query functionality of retrieving customer data for a specified customer_id. This change improves code clarity and readability.

--- 2- Retrieving Data From a Single Table/1- The SELECT Statement/1- The SELECT Statement.sql
+++ 2- Retrieving Data From a Single Table/1- The SELECT Statement/1- The SELECT Statement.sql
@@ -1,6 +1,5 @@
-USE sql_store;
-
 -- Select all columns from 'customers' table for customer with customer_id = 1, ordered by first_name.
+USE sql_store;
 SELECT *
 FROM customers
 WHERE customer_id = 1
