59a9ce0d00264dbb5626f7eca8624fe26f788243
Reacher <macryze@icloud.com>
2024-12-07 10:56:38 +0800

    Add 'use sql_store' statement before each SQL query
    This commit ensures that the 'use sql_store' statement is included before each SQL query to explicitly set the database context. This change enhances clarity and prevents errors when executing these queries on different sessions. It ensures consistent behavior across various execution environments.

--- 2- Retrieving Data From a Single Table/8- The REGEXP Operator/8- The REGEXP Operator.sql
+++ 2- Retrieving Data From a Single Table/8- The REGEXP Operator/8- The REGEXP Operator.sql
@@ -12,66 +12,79 @@ Summary:
 */
 
 -- Select customers whose last name contains 'field' (LIKE).
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name LIKE '%field%';
 
 -- Select customers whose last name contains 'field' (REGEXP).
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'field';
 
 -- Select customers whose last name starts with 'field'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP '^field';
 
 -- Select customers whose last name ends with 'field'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'field$';
 
 -- Select customers whose last name starts with 'field', 'mac', or 'rose'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP '^field|mac|rose';
 
 -- Select customers whose last name ends with 'field', 'mac', or 'rose'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'field$|mac|rose';
 
 -- Select customers whose last name contains 'ge', 'me', or 'ie'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP '[gim]e';
 
 -- Select customers whose last name contains 'ef', 'em', or 'eq'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'e[fmq]';
 
 -- Select customers whose last name contains any letter from 'a' to 'h' followed by 'e'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP '[a-h]e';
 
 -- Select customers whose first name contains 'elka' or 'ambur'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE first_name REGEXP 'elka|ambur';
 
 -- Select customers whose last name ends with 'ey' or 'on'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'ey$|on$';
 
 -- Select customers whose last name starts with 'my' or contains 'se'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP '^my|se';
 
 -- Select customers whose last name contains 'br' or 'bu'.
+use sql_store;
 SELECT *
 FROM customers
 WHERE last_name REGEXP 'b[ru]'; -- or using where last_name REGEXP 'br|bu'
