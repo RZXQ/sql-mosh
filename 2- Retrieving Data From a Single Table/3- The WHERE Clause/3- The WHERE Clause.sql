7b1db8f1630f14aa515396cc7e69d43824c83300
Reacher <macryze@icloud.com>
2024-12-07 09:49:14 +0800

    Add 'USE sql_store' to each query block in SQL script.
    This change ensures that each query is executed against the correct database by explicitly selecting the 'sql_store' database in each query block. This can prevent errors related to executing queries in an unintended database context and enhances script reliability when run in different environments.

--- 2- Retrieving Data From a Single Table/3- The WHERE Clause/3- The WHERE Clause.sql
+++ 2- Retrieving Data From a Single Table/3- The WHERE Clause/3- The WHERE Clause.sql
@@ -1,26 +1,29 @@
-USE sql_store;
-
 -- Select customers with points greater than 3000.
+USE sql_store;
 SELECT *
 FROM customers
 WHERE points > 3000;
 
 -- Select customers where the state is 'VA'.
+USE sql_store;
 SELECT *
 FROM customers
 WHERE state = 'VA';
 
 -- Select customers where the state is not 'VA'.
+USE sql_store;
 SELECT *
 FROM customers
 WHERE state != 'VA';
 
 -- Select customers born after January 1, 1990.
+USE sql_store;
 SELECT *
 FROM customers
 WHERE birth_date > '1990-01-01';
 
 -- Select orders placed on or after January 1, 2019.
+USE sql_store;
 SELECT *
 FROM orders
 WHERE order_date >= '2019-01-01';
