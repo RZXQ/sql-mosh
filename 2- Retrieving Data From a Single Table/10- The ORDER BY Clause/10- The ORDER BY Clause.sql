3c0d0c32966976f57e50cdb3b63aad1ed278acec
Reacher <macryze@icloud.com>
2024-12-07 11:33:17 +0800

    Add missing 'use sql_store' statements in SQL scripts
    This commit adds 'use sql_store;' at the beginning of each query block to ensure that the correct database context is set for all SQL operations. This change improves the reliability of the scripts by preventing errors related to database selection. It's particularly useful in environments where the default database might not be 'sql_store'.

--- 2- Retrieving Data From a Single Table/10- The ORDER BY Clause/10- The ORDER BY Clause.sql
+++ 2- Retrieving Data From a Single Table/10- The ORDER BY Clause/10- The ORDER BY Clause.sql
@@ -8,37 +8,44 @@ Summary:
 /* Default sorting order:
  * - The default sorting order is by the primary key if no ORDER BY clause is specified
  */
+use sql_store;
 SELECT *
 FROM customers;
 -- Select all columns from the customers table.
 
 -- Select customers sorted by first name in ascending order.
+use sql_store;
 SELECT *
 FROM customers
 ORDER BY first_name;
 
 -- Select customers sorted by first name in descending order.
+use sql_store;
 SELECT *
 FROM customers
 ORDER BY first_name DESC;
 
 -- Select customers sorted by state in descending order and first name in ascending order.
+use sql_store;
 SELECT *
 FROM customers
 ORDER BY state DESC, first_name;
 
 -- Select first name and last name of customers sorted by birth_date.
+use sql_store;
 SELECT first_name, last_name
 FROM customers
 ORDER BY birth_date;
 
 -- Select first name, last name, and points (constant value) of customers sorted by points and then by first name.
+use sql_store;
 SELECT first_name, last_name, 10 AS points
 FROM customers
 ORDER BY points, first_name;
 
 -- Select first name and last name of customers sorted by the first and second columns.
 -- Note: It is advised to avoid using positional ordering in production code.
+use sql_store;
 SELECT first_name, last_name
 FROM customers
 ORDER BY 1, 2;
@@ -48,6 +55,7 @@ ORDER BY 1, 2;
  */
 
 -- Select all columns and calculate total_price for order_items where order_id is 2, sorted by total_price in descending order.
+use sql_store;
 SELECT *, quantity * unit_price AS total_price
 FROM order_items
 WHERE order_id = 2
