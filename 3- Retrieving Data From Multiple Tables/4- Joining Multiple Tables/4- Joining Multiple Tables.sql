51e18ad5243ff94edd586d81db835383c94e2e74
Reacher <macryze@icloud.com>
2024-12-11 19:46:34 +0800

    Refactor SQL queries for clarity and consistency.
    Updated comments and formatting in SQL queries to improve readability and maintain consistency. Simplified descriptions to provide concise and clear context for retrieving relevant data.

--- 3- Retrieving Data From Multiple Tables/4- Joining Multiple Tables/4- Joining Multiple Tables.sql
+++ 3- Retrieving Data From Multiple Tables/4- Joining Multiple Tables/4- Joining Multiple Tables.sql
@@ -1,20 +1,19 @@
--- Selects 'order_id' and 'order_date' from 'orders' table
--- Selects 'first_name' and 'last_name' from 'customers' table for customer details
--- Selects 'name' from 'order_statuses' table as 'status' for order status description
--- Joins 'orders' table with 'customers' table from 'sql_store' database on condition: 'customer_id' matches
--- Joins 'orders' table with 'order_statuses' table from 'sql_store' database on condition: 'status' matches 'order_status_id'
+-- Query: Retrieve order details, customer details, and order status
+-- Select: order_id, order_date from orders; first_name, last_name from customers; name (as status) from order_statuses
+-- From: orders (o)
+-- Join: customers (c) on o.customer_id = c.customer_id
+-- Join: order_statuses (os) on o.status = os.order_status_id
 USE sql_store;
 SELECT o.order_id, o.order_date, c.first_name, c.last_name, os.name AS status
 FROM orders o
          JOIN customers c ON o.customer_id = c.customer_id
          JOIN order_statuses os ON o.status = os.order_status_id;
 
-
--- Selects 'date', 'invoice_id', and 'amount' from 'payments' table
--- Selects 'name' from 'clients' table for client name
--- Selects 'name' from 'payment_methods' table for payment method name
--- Joins 'payments' table with 'clients' table from 'sql_invoicing' database on condition: 'client_id' matches
--- Joins 'payments' table with 'payment_methods' table from 'sql_invoicing' database on condition: 'payment_method' matches 'payment_method_id'
+-- Query: Retrieve payment details, client name, and payment method
+-- Select: date, invoice_id, amount from payments; name from clients; name from payment_methods
+-- From: payments (p)
+-- Join: clients (c) on p.client_id = c.client_id
+-- Join: payment_methods (pm) on p.payment_method = pm.payment_method_id
 USE sql_invoicing;
 SELECT p.date, p.invoice_id, p.amount, c.name, pm.name
 FROM payments p
