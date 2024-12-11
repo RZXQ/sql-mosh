4212abda1ea57b315facad69cd3c8002338f9688
Reacher <macryze@icloud.com>
2024-12-11 20:23:39 +0800

    Fix SQL formatting by removing an unnecessary semicolon
    Removed an extra semicolon prior to the ORDER BY clause in the query. This change improves code readability and maintains consistent formatting throughout the file. No functional impact.

--- 3- Retrieving Data From Multiple Tables/8- Outer Join Between Multiple Tables/8- Outer Join Between Multiple Tables.sql
+++ 3- Retrieving Data From Multiple Tables/8- Outer Join Between Multiple Tables/8- Outer Join Between Multiple Tables.sql
@@ -8,8 +8,7 @@ USE sql_store;
 SELECT c.customer_id, c.first_name, o.order_id, sh.name AS shipper
 FROM customers c
          LEFT JOIN orders o ON c.customer_id = o.customer_id
-         LEFT JOIN shippers sh ON o.shipper_id = sh.shipper_id
-ORDER BY c.customer_id;
+         LEFT JOIN shippers sh ON o.shipper_id = sh.shipper_id;
 
 -- Query: Retrieve order details, customer name, shipper name, and order status
 -- Select: order_id, order_date from orders; first_name (as customer) from customers; name (as shipper) from shippers; name (as status) from order_statuses
