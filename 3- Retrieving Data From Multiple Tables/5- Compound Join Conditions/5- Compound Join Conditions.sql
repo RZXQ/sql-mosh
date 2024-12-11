716370fb84530e684df539905bb0c461086bce7d
Reacher <macryze@icloud.com>
2024-12-11 19:52:12 +0800

    Simplify JOIN condition formatting in SQL query.
    Combined multi-line JOIN condition into a single line for clarity and consistency. This improves readability and aligns the style with standard SQL formatting practices.

--- 3- Retrieving Data From Multiple Tables/5- Compound Join Conditions/5- Compound Join Conditions.sql
+++ 3- Retrieving Data From Multiple Tables/5- Compound Join Conditions/5- Compound Join Conditions.sql
@@ -6,6 +6,4 @@
 USE sql_store;
 SELECT *
 FROM order_items oi
-         JOIN order_item_notes oin
-              ON oi.order_id = oin.order_id
-                  AND oi.product_id = oin.product_id;
+         JOIN order_item_notes oin ON oi.order_id = oin.order_id AND oi.product_id = oin.product_id;
