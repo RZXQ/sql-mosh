b60e6a9f8d9cc8e1801a526b220422cfd5111d1b
Reacher <macryze@icloud.com>
2025-02-07 16:08:21 +0800

    Add detailed comments for IS NULL and IS NOT NULL queries
    Enhanced SQL queries with descriptive comments to improve clarity. Added explanations for filters, target tables, and the purpose of each query. This helps in understanding the use of IS NULL and IS NOT NULL operators.

--- 02- Retrieving Data From a Single Table/9- The IS NULL Operator/9- The IS NULL Operator.sql
+++ 02- Retrieving Data From a Single Table/9- The IS NULL Operator/9- The IS NULL Operator.sql
@@ -5,20 +5,35 @@ Summary:
    - IS NOT NULL: Checks if a column does not contain NULL values
 */
 
--- Select customers whose phone number is NULL.
-use sql_store;
+-- ------------------------------------------------------------------------------------
+-- Query 1: Select customers whose phone number is NULL
+-- Select From: 'customers' table
+-- Filter: phone IS NULL
+-- Note: `IS NULL` checks for records where the phone number is not provided
+-- ------------------------------------------------------------------------------------
+USE sql_store;
 SELECT *
 FROM customers
 WHERE phone IS NULL;
 
--- Select customers whose phone number is NOT NULL.
-use sql_store;
+-- ------------------------------------------------------------------------------------
+-- Query 2: Select customers whose phone number is NOT NULL
+-- Select From: 'customers' table
+-- Filter: phone IS NOT NULL
+-- Note: `IS NOT NULL` selects records where the phone number is available
+-- ------------------------------------------------------------------------------------
+USE sql_store;
 SELECT *
 FROM customers
 WHERE phone IS NOT NULL;
 
--- Select orders where shipped_date is NULL.
-use sql_store;
+-- ------------------------------------------------------------------------------------
+-- Query 3: Select orders where shipped_date is NULL
+-- Select From: 'orders' table
+-- Filter: shipped_date IS NULL
+-- Note: Checks for orders that have not been shipped (no shipped_date available)
+-- ------------------------------------------------------------------------------------
+USE sql_store;
 SELECT *
 FROM orders
 WHERE shipped_date IS NULL;
