6d3e725ee964ef75c777ea81a3106783489433e5
Reacher <macryze@icloud.com>
2025-02-13 14:54:21 +0800

    Update SQL comments for self outer joins clarity
    Improved comment structure to enhance readability and provide clearer explanations of query logic, including table aliases, joins, and conditions. This ensures better maintainability and understanding of the query purpose.

--- 03- Retrieving Data From Multiple Tables/9- Self Outer Joins/9- Self Outer Joins.sql
+++ 03- Retrieving Data From Multiple Tables/9- Self Outer Joins/9- Self Outer Joins.sql
@@ -1,9 +1,14 @@
 USE sql_hr;
 
--- Selects 'employee_id' and 'first_name' from 'employees' table as employee details,
--- and 'first_name' from 'employees' table as 'manager' to indicate each employee's manager's name
--- Joins 'employees' with itself using LEFT JOIN to include all employees, even those without a manager (e.g., the CEO)
--- On condition: 'reports_to' in the employee table matches 'employee_id' of the manager
+-- ------------------------------------------------------------------------------------
+-- Query: Retrieve employee details along with their respective managers' names
+-- Select: 'employee_id' and 'first_name' from 'employees' (as employee details)
+--          'first_name' from 'employees' (as 'manager' name)
+-- From: 'employees' table (aliased as 'e')
+-- Join: LEFT JOIN with 'employees' (aliased as 'm') to include all employees, even those without a manager (e.g., CEO)
+-- Condition: Matches 'reports_to' in 'employees' ('e') with 'employee_id' in 'employees' ('m')
+-- Note: The LEFT JOIN ensures that employees without a manager (null 'reports_to') are included
+-- ------------------------------------------------------------------------------------
 SELECT e.employee_id, e.first_name, m.first_name AS manager
 FROM employees e
          LEFT JOIN employees m ON e.reports_to = m.employee_id;
