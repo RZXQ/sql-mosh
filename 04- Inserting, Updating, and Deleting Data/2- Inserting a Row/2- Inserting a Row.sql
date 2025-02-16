8f3a3cd22617bf59ec7dfdcec6071df97b18262b
Reacher <macryze@icloud.com>
2025-02-17 02:11:04 +0800

    Fix SQL comment and add query to verify insertion
    Updated the comment to specify the correct keyword 'VALUES' and added a SELECT query to confirm the successful record insertion. This enhances documentation clarity and supports validation of the operation.

--- 04- Inserting, Updating, and Deleting Data/2- Inserting a Row/2- Inserting a Row.sql
+++ 04- Inserting, Updating, and Deleting Data/2- Inserting a Row/2- Inserting a Row.sql
@@ -1,8 +1,13 @@
+-- ------------------------------------------------------------------------------------
 -- Query: Insert a new customer record into the 'customers' table
 -- Insert Into: 'customers' table
 -- Columns: 'last_name', 'first_name', 'birth_date', 'address', 'city', 'state'
 -- Values: ('Smith', 'John', '1990-01-01', 'address', 'city', 'CA')
--- Note: Adds a new customer with specified details
+-- Note: Ensures correct syntax using 'VALUES' instead of 'VALUE'
+-- ------------------------------------------------------------------------------------
 USE sql_store;
 INSERT INTO customers (last_name, first_name, birth_date, address, city, state)
 VALUES ('Smith', 'John', '1990-01-01', 'address', 'city', 'CA');
+
+SELECT *
+FROM customers;
