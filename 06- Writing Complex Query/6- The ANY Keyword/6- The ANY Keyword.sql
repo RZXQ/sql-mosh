9017d395a3dc9b3842b4ac508980ab4b06655f70
Reacher <macryze@icloud.com>
2025-03-06 00:51:42 +0800

    Refactor comments for clarity in "ANY" and "IN" queries.
    Updated comments to provide clearer explanations of the queries retrieving clients with at least two invoices. Standardized structure and added consistent formatting for improved readability.

--- 06- Writing Complex Query/6- The ANY Keyword/6- The ANY Keyword.sql
+++ 06- Writing Complex Query/6- The ANY Keyword/6- The ANY Keyword.sql
@@ -1,18 +1,20 @@
+-- ------------------------------------------------------------------------------------
 -- Query: Retrieve clients with at least two invoices (using IN)
--- Select:
---   - All columns from `clients`
--- Filter: Includes rows where client_id is in the list of client_ids with at least two invoices
--- Note: Uses a subquery with GROUP BY and HAVING to find clients with two or more invoices
+-- Select: All columns from 'clients'
+-- Filter (WHERE): client_id IN (subquery identifying clients with >= 2 invoices)
+-- Note: Subquery uses GROUP BY and HAVING to find clients with two or more invoices
+-- ------------------------------------------------------------------------------------
 USE sql_invoicing;
 SELECT *
 FROM clients
 WHERE client_id IN (SELECT client_id FROM invoices GROUP BY client_id HAVING COUNT(*) >= 2);
 
+-- ------------------------------------------------------------------------------------
 -- Query: Retrieve clients with at least two invoices (using ANY)
--- Select:
---   - All columns from `clients`
--- Filter: Includes rows where client_id matches any client_id with at least two invoices
--- Note: Uses a subquery with GROUP BY and HAVING to identify qualifying client_ids
+-- Select: All columns from 'clients'
+-- Filter (WHERE): client_id = ANY (subquery identifying clients with >= 2 invoices)
+-- Note: Subquery uses GROUP BY and HAVING to identify qualifying client_ids
+-- ------------------------------------------------------------------------------------
 USE sql_invoicing;
 SELECT *
 FROM clients
