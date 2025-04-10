771d83333b8b0192e92893f37b18ebde59fb9001
Reacher <macryze@icloud.com>
2025-04-10 16:51:26 +0800

    Update IF logic and add subquery alternative for frequency
    Modified the IF condition in the first query to categorize years >= 2019. Added an alternative approach using a subquery to calculate order frequency for products.

--- 07- Essential MySQL Functions/7- The IF Function/7- The IF Function.sql
+++ 07- Essential MySQL Functions/7- The IF Function/7- The IF Function.sql
@@ -6,7 +6,7 @@
 -- From: 'orders' table (in `sql_store` database)
 -- Note: Uses IF to check if the year of order_date is 2019 and assigns 'Active' or 'Archived'
 USE sql_store;
-SELECT order_id, order_date, IF(YEAR(order_date) = '2019', 'Active', 'Archived') AS category
+SELECT order_id, order_date, IF(YEAR(order_date) >= '2019', 'Active', 'Archived') AS status
 FROM orders;
 
 -- Query: Retrieve product details and order frequency, categorized by count of orders
@@ -20,8 +20,18 @@ FROM orders;
 -- Group By: product_id, name
 -- Note: COUNT(*) calculates the number of orders per product, and IF categorizes
 --       products as 'Many times' if the count > 1, otherwise as 'Once'
+
+# a. using inner join
 USE sql_store;
 SELECT product_id, name, COUNT(*) AS orders, IF(COUNT(*) > 1, 'Many times', 'Once') AS frequency
 FROM products
          JOIN order_items USING (product_id)
 GROUP BY product_id, name;
+
+
+# b. using subquery
+SELECT product_id,
+       name,
+       IF((SELECT COUNT(*) FROM order_items oi WHERE oi.product_id = p.product_id) > 1, 'Many times',
+          'Once') AS frequence
+FROM products p;
