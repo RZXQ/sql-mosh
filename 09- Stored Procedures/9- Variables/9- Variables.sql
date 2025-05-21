4f08fe5f92ed0ac1bd846a3d1e090e063ba5150f
Reacher <macryze@icloud.com>
2025-05-21 15:21:28 +0800

    Rename variable for clarity and update references.
    Renamed `invoice_count` to `invoices_count` for better consistency and clarity. Adjusted all references and calculations accordingly to ensure functionality remains intact.

--- 09- Stored Procedures/9- Variables/9- Variables.sql
+++ 09- Stored Procedures/9- Variables/9- Variables.sql
@@ -4,15 +4,12 @@
 --   - Parameters: None
 --   - Local Variables:
 --     - `risk_factor` (DECIMAL(9,2)): Holds calculated result of the risk factor
---     - `invoice_count` (INT): Holds total count of invoices
+--     - `invoices_count` (INT): Holds total count of invoices
 --     - `invoices_total` (DECIMAL(9,2)): Holds total sum of invoice totals
 --   - Select: invoice count and total invoice amount
 --       - From: `invoices` table
---       - Calculation: risk_factor = invoices_total / invoice_count * 5
+--       - Calculation: risk_factor = invoices_total / invoices_count * 5
 USE sql_invoicing;
-
--- session variable
-SET @invoices_count = 0;
 
 DROP PROCEDURE IF EXISTS get_risk_factor;
 DELIMITER $$
@@ -20,17 +17,16 @@ CREATE PROCEDURE get_risk_factor()
 BEGIN
     # local variable
     DECLARE risk_factor DECIMAL(9, 2) DEFAULT 0;
-    DECLARE invoice_count INT;
+    DECLARE invoices_count INT;
     DECLARE invoices_total DECIMAL(9, 2);
 
     SELECT COUNT(*), SUM(invoice_total)
-    INTO invoice_count, invoices_total
+    INTO invoices_count, invoices_total
     FROM invoices;
 
-    SET risk_factor = invoices_total / invoice_count * 5;-- set value
+    SET risk_factor = invoices_total / invoices_count * 5;
     SELECT risk_factor;
 END$$
 DELIMITER ;
 
--- Call the stored procedure to calculate and retrieve the risk factor
 CALL get_risk_factor();
