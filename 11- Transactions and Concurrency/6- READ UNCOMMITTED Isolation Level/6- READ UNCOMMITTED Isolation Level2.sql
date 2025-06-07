80d0021900090f99d745a7ac9005ce159b28199d
Reacher <macryze@icloud.com>
2025-06-07 13:27:58 +0800

    Standardize SQL syntax in READ UNCOMMITTED isolation level scripts in Transactions and Concurrency module

--- 11- Transactions and Concurrency/6- READ UNCOMMITTED Isolation Level/6- READ UNCOMMITTED Isolation Level2.sql
+++ 11- Transactions and Concurrency/6- READ UNCOMMITTED Isolation Level/6- READ UNCOMMITTED Isolation Level2.sql
@@ -1,16 +1,18 @@
-use sql_store;
+USE sql_store;
 
 -- Start "rehearsal mode" - practicing behind the curtain
-start transaction;
+START TRANSACTION;
 
 -- Practice the scene (change points to 20, but audience can't see yet)
-update customers set points = 20 where customer_id = 1;
+UPDATE customers
+SET points = 20
+WHERE customer_id = 1;
 
 -- Decide "that was terrible!" - go back to original script
-rollback;
+ROLLBACK;
 
 -- This line does nothing now (you already went back to original)
-commit;
+COMMIT;
 
 -- What's my session ID? (different performer ticket)
 SELECT CONNECTION_ID();
