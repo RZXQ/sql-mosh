86fb3672502493a5ecde9c6b1dd491158a6239da
Reacher <macryze@icloud.com>
2025-06-08 08:15:04 +0800

    Add primary key setup scripts to Designing Databases module

---
+++ 13- Designing Databases/6- Primary Keys/6- Primary Keys.sql
@@ -0,0 +1,41 @@
+-- Table: students
+-- +--------------+--------------+----------+
+-- | Column Name  | Type         | Notes    |
+-- +--------------+--------------+----------+
+-- | student_id   | INT          | PK, AI   |
+-- | first_name   | VARCHAR(50)  | NOT NULL |
+-- | last_name    | VARCHAR(50)  | NOT NULL |
+-- | email        | VARCHAR(255) | NOT NULL |
+-- | dateRegistered | DATETIME   | NOT NULL |
+-- +--------------+--------------+----------+
+-- PK = Primary Key, AI = Auto Increment
+
+CREATE TABLE students
+(
+    student_id     INT AUTO_INCREMENT PRIMARY KEY,
+    first_name     VARCHAR(50)  NOT NULL,
+    last_name      VARCHAR(50)  NOT NULL,
+    email          VARCHAR(255) NOT NULL,
+    dateRegistered DATETIME     NOT NULL
+);
+
+-- Table: courses
+-- +-------------+--------------+----------+
+-- | Column Name | Type         | Notes    |
+-- +-------------+--------------+----------+
+-- | course_id   | INT          | PK, AI   |
+-- | title       | VARCHAR(255) | NOT NULL |
+-- | price       | DECIMAL(5,2) | NOT NULL |
+-- | instructor  | VARCHAR(255) | NOT NULL |
+-- | tags        | VARCHAR(255) | NOT NULL |
+-- +-------------+--------------+----------+
+-- PK = Primary Key, AI = Auto Increment
+
+CREATE TABLE courses
+(
+    course_id  INT AUTO_INCREMENT PRIMARY KEY,
+    title      VARCHAR(255)  NOT NULL,
+    price      DECIMAL(5, 2) NOT NULL,
+    instructor VARCHAR(255)  NOT NULL,
+    tags       VARCHAR(255)  NOT NULL
+);
