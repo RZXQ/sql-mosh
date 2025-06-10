f5fe80ae5b82ce6815b6716420b97cd0f68b7b45
Reacher <macryze@icloud.com>
2025-06-10 17:29:13 +0800

    Remove `last_name` column from `customers` table creation script in Designing Databases module

--- 13- Designing Databases/26- Creating Tables/26- Creating Tables.sql
+++ 13- Designing Databases/26- Creating Tables/26- Creating Tables.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS customers
 (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name  VARCHAR(50)  NOT NULL,
-    last_name   VARCHAR(50)  NOT NULL,
     points      INT          NOT NULL DEFAULT 0,
     email       VARCHAR(255) NOT NULL UNIQUE
 )
