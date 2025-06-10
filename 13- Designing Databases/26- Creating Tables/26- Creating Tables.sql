b7ad31852334df0a3805b4e7c3a8e543e0b7790b
Reacher <macryze@icloud.com>
2025-06-10 16:10:56 +0800

    Add script for creating `customers` table in Designing Databases module

---
+++ 13- Designing Databases/26- Creating Tables/26- Creating Tables.sql
@@ -0,0 +1,12 @@
+USE sql_store2;
+
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE IF NOT EXISTS customers
+(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name  VARCHAR(50)  NOT NULL,
+    last_name   VARCHAR(50)  NOT NULL,
+    points      INT          NOT NULL DEFAULT 0,
+    email       VARCHAR(255) NOT NULL UNIQUE
+)
