b5533aca13bdfa4397a8a6b2cc142b85224e6beb
Reacher <macryze@icloud.com>
2025-06-14 15:13:19 +0800

    Add script for viewing users in Securing Databases module

---
+++ 15- Securing Databases/3- Viewing Users/3- Viewing Users.sql
@@ -0,0 +1,5 @@
+-- Way 1: View users using query
+select * from mysql.user;
+
+-- Way 2: View users using WebStorm navigator panel on the right side
+-- Note: root user has host 'localhost', meaning connections only from the same machine
